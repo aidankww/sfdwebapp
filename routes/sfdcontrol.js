c2dd726239be153385f9cd223e7523011909afeb
Aidan Kovacic <aidankovacic@gmail.com>
2020-02-19 16:53:46 -0500

    First Commit

---
+++ routes/sfdcontrol.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs')
+
+router.post('/', function(req, res, next ) {
+    var message = req.body.text;
+    var author = req.body.user_name;
+    let messageObject = {
+        author: author,
+        message: message
+    };
+    let data = JSON.stringify(messageObject);
+    var log = author + ": " + message + "\n";
+    res.send("Message queued.");
+    fs.writeFile('../command.json', data, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+    fs.appendFile('./log.txt', log, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+  });
+
+router.get('/', function(req, res, next) {
+    fs.readFile('./command.json', (err, data) => {
+        if (err) throw err;
+        let message = JSON.parse(data);
+        res.json(message);
+        console.log("[SFD] GET request received!");
+    });
+});
+
+module.exports = router;
